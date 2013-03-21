babfe19a67771e5b17f0eac9b5dbe3ffe6b365fa
junzhe <junzhe@umich.edu>
2013-03-20 22:48:02 -0400

    final

--- Utility.h
+++ Utility.h
@@ -13,14 +13,14 @@ struct Error {
 	const char * msg;
 };
 
-/* add any of your own declarations here */
-
+//Sim_object comparator based on name
 struct Sim_object_Compare{
   bool operator() (const Sim_object* o1, const Sim_object* o2){
     return o1->get_name().compare(o2->get_name())<0;
   }
 };
 
+//Sim_object comparator for name exist
 struct Sim_object_Name_Compare_with_string{
   bool operator() (const Sim_object* o, const string& s){
     string name = o->get_name();
@@ -28,12 +28,14 @@ struct Sim_object_Name_Compare_with_string{
   }
 };
 
+//Sim_object comparator with a name string
 struct Sim_object_Compare_with_string{
   bool operator() (const Sim_object* o, const string& s){
     return o->get_name().compare(s)==0;
   }
 };
 
+//Functor for remove a sim object
 struct Sim_object_Remove{
   void operator() (Sim_object* o){
     delete o;
